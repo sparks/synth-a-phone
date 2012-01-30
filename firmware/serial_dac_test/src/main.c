5a8ab55198792e0cec519c5d993ed4ff11e2c124
Severin Smith <info@smallbutdigital.com>
2012-01-29 19:47:06 -0500

    Touch up

--- firmware/serial_dac_test/src/main.c
+++ firmware/serial_dac_test/src/main.c
@@ -13,20 +13,22 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+volatile uint8_t too_slow_flag = 0;
 volatile uint8_t compute_flag = 0;
 uint16_t output = 0;
 
 int main(void) {
 	adc_init();
 	dac_init();
-	timer_init(&trigger_computation, 0x26);
+	timer_init(&trigger_computation, 0x50);
 
 	sei();
 
 	for(;;) {
 		if(compute_flag != 0) {
-			// output = sawtooth((adc_val(0) >> 2) + 1);
-			output = triangle((adc_val(0) >> 2) + 1);
+			output = 0;
+			output += sawtooth((adc_val(0) >> 2) + 1) >> 1;
+			output += triangle((adc_val(0) >> 2) + (adc_val(1) >> 2) + 1) >> 1;
 			compute_flag = 0;
 		}
 	}
@@ -35,7 +37,10 @@ int main(void) {
 }
 
 void trigger_computation(void) {
-	// serial_dac(output);
-	if(!is_lossy()) serial_dac(output);
-	compute_flag = 1;
+	if(compute_flag != 0) too_slow_flag = 1;
+
+	if(!is_lossy() && too_slow_flag == 0) {
+		serial_dac(output);
+		compute_flag = 1;
+	}
 }
