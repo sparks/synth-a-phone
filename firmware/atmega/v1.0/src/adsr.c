35efd8da15f65b2a13698ee9b0d7fa862800c044
Severin Smith <info@smallbutdigital.com>
2012-03-05 21:10:00 -0500

    Switching to ints

--- firmware/atmega/v1.0/src/adsr.c
+++ firmware/atmega/v1.0/src/adsr.c
@@ -5,8 +5,8 @@
 
 #include "adsr.h"
 
-uint8_t times[5] = {0, 5, 25, 26, 126};
-int8_t levels[5] = {0, 127, 60, 30, 0};
+uint8_t times[5] = {0, 40, 80, 120, 160};
+uint8_t levels[5] = {0, 255, 255, 255, 0};
 
 uint8_t gate = 0;
 uint8_t cur_time = 0xFF;
@@ -17,16 +17,18 @@ void trig_gate(uint8_t val) {
 }
 
 uint8_t adsr_value(void) {
-	if(cur_time >= times[3]) return 0;
-	else {
-		int8_t value = 0;
+	if(cur_time >= times[4] && gate == 0) {
+		return 0;
+	} else {
+		uint8_t value = 0;
 		uint8_t i;
-		for(i = 0;i < 4;i++) {
+		for(i = 0;i < 5;i++) {
 			if(cur_time < times[i]) {
-				value = (levels[i]-levels[i-1])*(times[i]-cur_time)/(times[i]-times[i-1]);
+				value = levels[i]-(levels[i]-levels[i-1])*(times[i]-cur_time)/(times[i]-times[i-1]);
+				break;
 			}
 		}
-		if((gate == 1 && cur_time < times[2]) || (gate == 0 && cur_time < times[3])) cur_time++;
-		return 0;
+		if((gate == 1 && cur_time < times[3]) || (gate == 0 && cur_time < times[4])) cur_time++;
+		return value;
 	}
 }
--- firmware/atmega/v1.0/src/osc.h
+++ firmware/atmega/v1.0/src/osc.h
@@ -10,9 +10,9 @@
 
 void sine_init(void);
 
-uint16_t sawtooth(uint16_t freq);
-uint16_t triangle(uint16_t freq);
-uint16_t pulse(uint16_t freq);
-uint16_t sine(uint16_t freq);
+int16_t sawtooth(uint16_t freq);
+int16_t triangle(uint16_t freq);
+int16_t pulse(uint16_t freq);
+int16_t sine(uint16_t freq);
 
 #endif
