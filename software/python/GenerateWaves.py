82b9b97aef0f40ee3d6b09a6baac9e6e00f8f9b7
Benzor123 <stephane.beniak@gmail.com>
2012-03-12 17:13:31 -0400

    GenerateWaves.py no longer takes a desired frequency, since these may not result in tables being a proper multiple of the period. Instead, give it a number of octaves, and it'll generate a wavetable for each of those (doubling frequency each time, which is also a period multiple of the original wave).

--- software/python/GenerateWaves.py
+++ software/python/GenerateWaves.py
@@ -5,39 +5,89 @@ import matplotlib.pyplot as plot
 parser = argparse.ArgumentParser(description='Generate wave tables for sine, triangle, sawtooth, and square waves, given the inputs specified.')
 parser.add_argument('-l', '--length', action='store', dest='len', type=int, help='length of the wave table.')
 parser.add_argument('-a', '--amplitude', action='store', dest='amp', type=int, help='amplitude of the output waves.')
-parser.add_argument('-f', '--frequency', action='store', dest='freq', type=int, help='frequency of the output waves.')
+parser.add_argument('-o', '--octaves', action='store', dest='oct', type=int, help='number of octaves to generate tables for.')
 parser.add_argument('-s', '--samplerate', action='store', dest='samp', type=int, help='sampling rate of the device.')
 
 args = parser.parse_args()
 
 len = args.len
 amp = args.amp
-freq = args.freq
+oct = args.oct
 samp = args.samp
+
+freq = samp / len
 
 wave_period = 1 / freq
 samp_period = 1 / samp
+
+octaves = []
+
+for i in range(oct):
+	octaves.append(math.pow(2, i))
 
 sine = []
 triangle = []
 sawtooth = []
 square = []
 
-for i in range(len):
-	sine.append(amp * math.sin(i * (samp_period / wave_period) * 2 * math.pi))
-	triangle.append(4 * amp * (samp_period / wave_period) * -math.fabs((i % (wave_period / samp_period)) - (wave_period / (2 * samp_period))) + amp)
-	sawtooth.append((2 * amp * i * (samp_period / wave_period) % (2 * amp)) - amp)
-	square.append(amp if ((i % (wave_period / samp_period)) < (wave_period / (2 * samp_period))) else -amp) 
+for j in range(oct):
+	sine.append([])
+	triangle.append([])
+	sawtooth.append([])
+	square.append([])
+	for i in range(len):
+		sine[j].append(amp * math.sin(octaves[j] * i * (samp_period / wave_period) * 2 * math.pi))
+		triangle[j].append(4 * amp * (samp_period / wave_period) * -math.fabs(((octaves[j] * i) % (wave_period / samp_period)) - (wave_period / (2 * samp_period))) + amp)
+		sawtooth[j].append((2 * amp * octaves[j] * i * (samp_period / wave_period) % (2 * amp)) - amp)
+		square[j].append(amp if (((octaves[j] * i) % (wave_period / samp_period)) < (wave_period / (2 * samp_period))) else -amp) 
 
-plot.subplot(221)
-plot.plot(sine, 'b')
+# If generating at least 4 octaves, you can uncomment everything below to visualize the wavetables as a plot
 
-plot.subplot(222)
-plot.plot(triangle, 'g')
+# plot.subplot(4, 4, 1)
+# plot.plot(sine[0], 'b.')
 
-plot.subplot(223)
-plot.plot(sawtooth, 'r')
+# plot.subplot(4, 4, 2)
+# plot.plot(sine[1], 'b.')
 
-plot.subplot(224)
-plot.plot(square, 'k')
-plot.show()
+# plot.subplot(4, 4, 3)
+# plot.plot(sine[2], 'b.')
+
+# plot.subplot(4, 4, 4)
+# plot.plot(sine[3], 'b.')
+
+# plot.subplot(4, 4, 5)
+# plot.plot(triangle[0], 'g')
+
+# plot.subplot(4, 4, 6)
+# plot.plot(triangle[1], 'g')
+
+# plot.subplot(4, 4, 7)
+# plot.plot(triangle[2], 'g')
+
+# plot.subplot(4, 4, 8)
+# plot.plot(triangle[3], 'g')
+
+# plot.subplot(4, 4, 9)
+# plot.plot(sawtooth[0], 'r')
+
+# plot.subplot(4, 4, 10)
+# plot.plot(sawtooth[1], 'r')
+
+# plot.subplot(4, 4, 11)
+# plot.plot(sawtooth[2], 'r')
+
+# plot.subplot(4, 4, 12)
+# plot.plot(sawtooth[3], 'r')
+
+# plot.subplot(4, 4, 13)
+# plot.plot(square[0], 'k')
+
+# plot.subplot(4, 4, 14)
+# plot.plot(square[1], 'k')
+
+# plot.subplot(4, 4, 15)
+# plot.plot(square[2], 'k')
+
+# plot.subplot(4, 4, 16)
+# plot.plot(square[3], 'k')
+# plot.show()
