5c8ca5893a1058ce1f9fba360aa970fc2538dff2
Severin Smith <info@smallbutdigital.com>
2012-03-28 01:32:55 -0400

    Fourth order feedback

--- software/python/feedback.py
+++ software/python/feedback.py
@@ -2,18 +2,18 @@ from pylab import *
 from scipy.signal import *
 from numpy.polynomial import *
 
-B, A = [0, 0, 1, 1], polypow([1, 1], 6)
+B, A = array([0, 0, 0, 0, 1]), array(polypow([1, 1], 4))
 
-for k in range(1, 10):
+print B, A
+
+for i in range(5):
+	k = i
 	Ak = polyadd(A, k*B)
 	resp = freqs(B, Ak)
 	plot(resp[0], abs(resp[1]), label="k="+str(k))
 
-# plot([1, 1], [1e-5, 1])
-# plot(resp[0], [1/10**(3.0/20) for i in resp[0]])
-
 xscale('log')
-# yscale('log')
+yscale('log')
 legend()
 
 show()
