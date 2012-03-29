4cdca8cc01414fa0a664cc02952721f2efe5c4eb
Severin Smith <info@smallbutdigital.com>
2012-03-29 17:22:16 -0400

    Just playing around with amplifier design

--- software/python/feedback.py
+++ software/python/feedback.py
@@ -2,15 +2,26 @@ from pylab import *
 from scipy.signal import *
 from numpy.polynomial import *
 
-B, A = array([0, 0, 0, 0, 1]), array(polypow([1, 1], 4))
+def feedback_amp():
+	B, A = array([0, 0, 0, 0, 1]), array(polypow([1, 1], 4))
 
-print B, A
+	for i in range(5):
+		k = i
+		Ak = polyadd(A, k*B)
+		resp = freqs(B, Ak)
+		plot(resp[0], abs(resp[1]), label="k="+str(k))
 
-for i in range(5):
-	k = i
-	Ak = polyadd(A, k*B)
-	resp = freqs(B, Ak)
-	plot(resp[0], abs(resp[1]), label="k="+str(k))
+def exact_vs_approx():
+	B1, A1 = [0, 1], [-1, 1]
+	B2, A2 = [-1.4, 1], [-1, 1]
+
+	resp = freqs(B1, A1)
+	plot(resp[0], abs(resp[1]), label="normal")
+
+	resp = freqs(B2, A2)
+	plot(resp[0], abs(resp[1]), label="diff")
+
+feedback_amp()
 
 xscale('log')
 yscale('log')
