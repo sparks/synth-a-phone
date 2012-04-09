2c1e910f42b14a48bfdce360303f93938bacf130
Severin Smith <info@smallbutdigital.com>
2012-04-09 15:00:52 -0400

    Working working

--- software/python/VCF.py
+++ software/python/VCF.py
@@ -2,24 +2,37 @@ from pylab import *
 from scipy.signal import *
 from numpy.polynomial import *
 
-print butter(4, 1)
+def filter(Iabc, label):
 
-C2, C3, C4 = 100.0e-12, 200.0e-12, 470.0e-12 #Order 2 complex conjugate poles
-Iabc = 2e-3/4 #Range 500uA to 2mA
-print Iabc
-gm = 19.6*Iabc
-alpha = 220.0/(100.0e3+220.0)
-beta = gm*alpha
+	C1, C2, C3, C4 = 100.0e-12, 200.0e-12, 400.0e-12, 800.0e-12  #Order 2 complex conjugate poles
+	# Iabc = 2e-5 #Range 500uA to 2mA
+	gm = 19.6*Iabc
+	alpha = 220.0/(100.0e3+220.0)
+	beta = gm*alpha
 
-B, A = [1], [C2*C3*C4/beta**3, C3*C4/beta**2, C4/beta, 1]
+	B, A = array([0, 0, 0, 0, 1]), array([C1*C2*C3*C4/beta**4, C2*C3*C4/beta**3, C3*C4/beta**2, C4/beta, 1])
 
-poles = roots(A)
-plot(real(poles), imag(poles), 'x')
-show()
+	# poles = roots(A)
+	# plot(real(poles), imag(poles), 'x')
+	# show()
 
-resp = freqs(B, A)
-plot(resp[0], abs(resp[1]))
+	for i in range(5):
+		k = i
+		Ak = polyadd(A, k*B)
+		resp = freqs(B, Ak)
+		if label == 1: plot(resp[0], abs(resp[1]), label="k="+str(k))
+		else: plot(resp[0], abs(resp[1]))
 
+	# resp = freqs(B, A)
+	# plot(resp[0], abs(resp[1]))
+
+filter(2e-7, 0)
+filter(2e-5, 0)
+filter(2e-3, 0)
+
+title("4th Order VCF feedback effects")
+legend()
 xscale('log')
 yscale('log')
+
 show()
