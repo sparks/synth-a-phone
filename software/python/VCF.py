9794ff9db731cab9881e7ba059a1873011997b33
Severin Smith <info@smallbutdigital.com>
2012-04-09 13:14:37 -0400

    Added spice simulation prelim and VCF stuff

--- software/python/VCF.py
+++ software/python/VCF.py
@@ -4,13 +4,14 @@ from numpy.polynomial import *
 
 print butter(4, 1)
 
-C1, C2, C3, C4 = 100.0e-12, 200.0e-12, 400.0e-12, 800.0e-12 #Order 2 complex conjugate poles
-Iabc = 1e-3 #Range 500uA to 2mA
-gm = 19.2*Iabc
-alpha = 200.0/(100.0e3+200.0)
+C2, C3, C4 = 100.0e-12, 200.0e-12, 470.0e-12 #Order 2 complex conjugate poles
+Iabc = 2e-3/4 #Range 500uA to 2mA
+print Iabc
+gm = 19.6*Iabc
+alpha = 220.0/(100.0e3+220.0)
 beta = gm*alpha
 
-B, A = [1], [C1*C2*C3*C4/beta**4, C2*C3*C4/beta**3, C3*C4/beta**2, C4/beta, 1]
+B, A = [1], [C2*C3*C4/beta**3, C3*C4/beta**2, C4/beta, 1]
 
 poles = roots(A)
 plot(real(poles), imag(poles), 'x')
