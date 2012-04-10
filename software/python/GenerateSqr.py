c2758190c8ff91670a5fa5a709c9e76e38703d0d
ssalenik <stepan.salenikovich@gmail.com>
2012-04-09 23:33:27 -0400

    bandlimitted square generator script

---
+++ software/python/GenerateSqr.py
@@ -0,0 +1,44 @@
+import math
+
+base_freq = [25.96, 41.20, 65.41, 103.83, 164.81, 261.62, 415.30, 659.25, 1046.50, 1661.21, 2637.01, 4185.98]
+max_limit = [0.81, 0.82, 0.81, 0.85, 0.92, 0.93, 0.94, 0.93, 0.94, 0.95, 0.97, 1.00]
+max_amp = 2047
+length = 256
+sampling_freq = 31250
+
+tables = []
+
+for i in range(len(base_freq)):
+	tables.append([])
+	max_k = int(round(sampling_freq/2 -base_freq[i]))
+	#print(max_k)
+	amp = int(round(max_amp*max_limit[i]))
+	for j in range(length):
+		#print(j)
+		term = 0.0
+		for k in range(1,max_k):
+			#print(k)
+			if k%2  != 0:
+				term += (4/(math.pi * k))*math.sin(2*math.pi*k*j/length)
+		tables[i].append((int(round((amp*term)))))
+
+f = open('wavetable_sqr' + str(length) + '.h', 'w')
+
+f.write('#include <avr/pgmspace.h>\n\n')
+
+
+f.write('int16_t const sqr_lookup[' + str(len(base_freq)) + '][' + str(length) + '] PROGMEM =')
+f.write('\n')
+f.write('{')
+for i in range(len(base_freq)):
+	f.write('{' + str.strip(str(tables[i]), '[]') + '}')
+	if i != len(base_freq) - 1 :
+		f.write(',')
+	f.write('\n')
+f.write('};')
+f.write('\n')
+	
+f.write('\n')
+f.close()
+				
+		
